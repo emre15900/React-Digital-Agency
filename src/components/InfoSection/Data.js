25a73f533bcc91b64c76ddf574352be1930632af
emre15900 <79264023+emre15900@users.noreply.github.com>
2021-08-26 16:24:53 +0300

    Img Data

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,53 @@
+export const homeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Premium Bank",
+  headLine: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
+  buttonLabel: "Get started",
+  imgStart: false,
+  img: "https://emre.sayinweb.com/src/svg-1.svg",
+  alt: "Car",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
+export const homeObjTwo = {
+  id: "about",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Premium Bank",
+  headLine: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
+  buttonLabel: "Get started",
+  imgStart: true,
+  img: "https://emre.sayinweb.com/src/svg-3.svg",
+  alt: "Car",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjThree = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Premium Bank",
+  headLine: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
+  buttonLabel: "Get started",
+  imgStart: false,
+  img: "https://emre.sayinweb.com/src/svg-6.svg",
+  alt: "Car",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
--- src/components/InfoSection/InfoElements.js
+++ src/components/InfoSection/InfoElements.js
@@ -12,7 +12,7 @@ export const InfoContainer = styled.div`
 export const InfoWrapper = styled.div`
   display: grid;
   z-index: 1;
-  height: 860px;
+  height: 700px;
   width: 100%;
   max-width: 1100px;
   margin-right: auto;
@@ -86,11 +86,13 @@ export const BtnWrap = styled.div`
   display: flex;
   justify-content: flex-start;
 `;
+
 export const ImgWrap = styled.div`
   max-width: 555px;
   height: 100%;
 `;
-export const Img = styled.div`
+
+export const Img = styled.img`
   width: 100%;
   margin: 0 0 10px 0;
   padding-right: 0;
