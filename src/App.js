8fd0ffd5c18ae44a47ec5efcf8567437a2157fc0
emre15900 <79264023+emre15900@users.noreply.github.com>
2021-09-02 13:34:43 +0300

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import './App.css';
-import {BrowserRouter as Router, Switch} from "react-router-dom"
-import Home from './pages';
-import SigninPage from "./pages/signin"
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages";
+import SigninPage from "./pages/signin";
 
 function App() {
   return (
